a7bc13c0a804578d376eafdf1cea9f272448b021
Mithun Prabhu <prabhuofficial7@gmail.com>
2021-05-30 18:35:23 +0530

    process added

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(4000, () => {
+const port = process.env.port || 4000;
+
+app.listen(port, () => {
     console.log("Server running");
 })
